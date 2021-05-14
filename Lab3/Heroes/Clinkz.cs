51c34991be72a3c2c0abf0fe4a8704d36db875e2
Kimishu <46163424+Kimishu@users.noreply.github.com>
2021-05-14 21:33:31 +0300

    Add files via upload
    Lab3&5&6

---
+++ Lab3/Heroes/Clinkz.cs
@@ -0,0 +1,26 @@
+﻿
+namespace Lab3
+{
+    class Clinkz : HeroType
+    {
+        public override double AtrStrength => 2.2;
+        public override double AtrAgility => 2.7;
+        public override double AtrIntelligence => 2.2;
+        public override double MainAtr => 2.7;
+        public Clinkz(HeroData data)
+        {
+            Gold = data.Gold;
+            Level = data.Level;
+            HeroName = data.HeroName;
+            Armor = data.Armor;
+            HP = data.HP;
+            Mana = data.Mana;
+            Strength = data.Strength;
+            Agility = data.Agility;
+            Intelligence = data.Intelligence;
+            MainAtribute = new AttributeResolver(2);
+            AttackType = "Дальний бой";
+            Attack = data.Attack;
+        }
+    }
+}
---
+++ Lab3/Heroes/Tidehunter.cs
@@ -0,0 +1,26 @@
+﻿
+namespace Lab3
+{
+    class Tidehunter : HeroType
+    {
+        public override double AtrStrength => 3.5;
+        public override double AtrAgility => 1.5;
+        public override double AtrIntelligence => 1.7;
+        public override double MainAtr => 3.5;
+        public Tidehunter(HeroData data)
+        {
+            Gold = data.Gold;
+            Level = data.Level;
+            HeroName = data.HeroName;
+            Armor = data.Armor;
+            HP = data.HP;
+            Mana = data.Mana;
+            Strength = data.Strength;
+            Agility = data.Agility;
+            Intelligence = data.Intelligence;
+            MainAtribute = new AttributeResolver(1);
+            AttackType = "Ближний бой";
+            Attack = data.Attack;
+        }
+    }
+}
